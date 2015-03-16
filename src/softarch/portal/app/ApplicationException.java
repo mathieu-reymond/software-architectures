2a6d3f8d033b8a86eeb067fc63ce88702ec1176d
Mathieu Reymond <mathieu.reymond@gmail.com>
2015-03-16 13:40:33 +0100

    initial commit

---
+++ src/softarch/portal/app/ApplicationException.java
@@ -0,0 +1,14 @@
+package softarch.portal.app;
+
+
+/**
+ * @author Niels Joncheere
+ */
+public class ApplicationException extends Exception {
+	/**
+	 * Creates a new application exception.
+	 */
+	public ApplicationException(String message) {
+		super(message);
+	}
+}
